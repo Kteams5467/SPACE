eaf49fd6a1adec7a46e12efb5c5c6aa11d01e690
Frapona <152498532+Frapona@users.noreply.github.com>
2024-05-22 13:44:38 +0200

    Spostati i file in una cartella unica
    in questo modo bastera installare una cartella per poter giocare e il programma si basera su di essa all'avvio

---
+++ spaceInvaders/include/Punteggio.h
@@ -0,0 +1,20 @@
+#ifndef PUNTEGGIO_H
+#define PUNTEGGIO_H
+
+void salvaPunteggio(int punteggio, int punteggioMax);
+
+#endif
+
+#ifndef BEST_SCORE_H
+#define BEST_SCORE_H
+
+int caricaPunteggio();
+
+#endif
+
+#ifndef CANCELLA_SCORE_H
+#define CANCELLA_SCORE_H
+
+void cancellaScore();
+
+#endif
---
+++ spaceInvaders/include/fineGioco.h
@@ -0,0 +1,6 @@
+#ifndef FINEGIOCO_H
+#define FINEGIOCO_H
+
+void fineGioco(int vincita, int punteggio, int punteggioMax);
+
+#endif
