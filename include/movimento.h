19efaf6637d4b8ea456c5e168ee404d1d8799f22
Frapona <152498532+Frapona@users.noreply.github.com>
2024-05-22 13:40:37 +0200

    Aggiunti file headers
    trasportata la cartella include da replit

---
+++ include/movimento.h
@@ -0,0 +1,6 @@
+#ifndef MOVIMENTO_H
+#define MOVIMENTO_H
+
+int movimenti(int *punteggio, int *vincita, int *punteggioMax, int *opzioni_generali, int *opzioni_alieni, int *opzioni_proiettili, int *opzioni_bombe, int *opzioni_casualitaBomba);
+
+#endif
